1cf9a2da7a7817adf1bb2f2d6ad0f0789de8c524
ripeals <ripeals@hotmail.com>
2026-04-23 13:01:33 +0100

    chore: infrastructure init setup (#3)
    * chore: configure azurerm provider
    
    * chore: add .env and .terraform to .gitignore
    
    * chore: add tfvars files for dev and prod environments
    
    * fix: terraform format

---
+++ infra/terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.13"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.69.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
