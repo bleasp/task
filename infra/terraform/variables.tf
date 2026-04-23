06efe7368c62fda4449328368a2251fa84e682a2
Rita <ripeals@hotmail.com>
2026-04-23 19:14:56 +0100

    chore: add tfstate to .gitignore

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,23 +1,23 @@
 variable "app_name" {
-  description = "value"
+  description = "Name of the application used for naming Azure resources."
   type        = string
   default     = "albums"
-
 }
+
 variable "environment" {
-  description = ""
+  description = "Deployment environment for the resources (e.g., dev, prod)."
   type        = string
   default     = "dev"
 }
 
 variable "location" {
-  description = "The location where resources will be created."
+  description = "Azure location where all resources will be deployed."
   type        = string
   default     = "West Europe"
 }
 
 variable "tags" {
+  description = "A map of key-value pairs used to tag resources for organization."
   type        = map(string)
-  description = ""
   default     = {}
 }
