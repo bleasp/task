577ae50982095f22190e21825db0a577086ae230
Rita <ripeals@hotmail.com>
2026-04-23 21:29:14 +0100

    feat: add configuration to create cae and ca

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -23,3 +23,43 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags     = local.tags
 }
+
+resource "azurerm_container_app_environment" "cae" {
+  name                = "cae-${var.app_name}-${var.environment}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags
+}
+
+resource "azurerm_role_assignment" "role" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_container_app.ca.identity[0].principal_id
+}
+
+resource "azurerm_container_app" "ca" {
+  name                         = "ca-${var.app_name}-${var.environment}"
+  container_app_environment_id = azurerm_container_app_environment.cae.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+  tags                         = local.tags
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  template {
+    container {
+      name   = "${var.app_name}-app"
+      image  = "mcr.microsoft.com/k8se/quickstart:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      template[0].container[0].image
+    ]
+  }
+}
