24cd28e01849951b3dd5d27628c91bb77a9c726a
Rita <ripeals@hotmail.com>
2026-04-24 08:24:27 +0100

    chore: add missing resources

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -15,9 +15,26 @@ variable "location" {
   type        = string
   default     = "West Europe"
 }
+
 variable "image_tag" {
   type        = string
   description = "The image tag/SHA to deploy. Passed from the GitHub Actions CI/CD pipeline."
+}
+
+variable "ca_config" {
+  type = object({
+    min_replicas = number
+    max_replicas = number
+    cpu          = string
+    memory       = string
+  })
+  default = {
+    min_replicas = 1
+    max_replicas = 2
+    cpu          = 0.25
+    memory       = "0.5Gi"
+  }
+  description = "Specific configuration for the target environment container app."
 }
 
 
